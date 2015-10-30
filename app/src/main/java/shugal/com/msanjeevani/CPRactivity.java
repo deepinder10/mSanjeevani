23deda4b0007e6cf25ed44485017615db77b4c22
Abhishek Raina <araina97@gmail.com>
2015-10-30 21:00:30 +0530

    Work Prototype

--- app/src/main/java/shugal/com/msanjeevani/CPRactivity.java
+++ app/src/main/java/shugal/com/msanjeevani/CPRactivity.java
@@ -15,6 +15,8 @@ public class CPRactivity extends AppCompatActivity {
         setContentView(R.layout.activity_cpractivity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("CPR");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
